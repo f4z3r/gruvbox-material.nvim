e52312473a74321487ec8d5d3c83ce07467ed9e3
Ruslan <kros8925@gmail.com>
2021-09-06 15:48:00 +0300

    Set default global options.

--- lua/gruvbox-material/init.lua
+++ lua/gruvbox-material/init.lua
@@ -1,14 +1,44 @@
 local utils = require('gruvbox-material.utils')
 local highlights = require('gruvbox-material.highlights')
 
+local g = vim.g
+
+function set_default_global_options()
+  local opts = {
+    diagnostic_text_highlight = 0,
+    diagnostic_virtual_text = 'grey',
+    enable_italic_comment = 1,
+    sign_column_background = 'none',
+    transparent_background = 0
+  }
+
+  for key, val in pairs(opts) do
+    -- add prefix
+    local key = 'gruvbox_material_' .. key
+
+    if vim.g[val] == nil then 
+      vim.g[key] = val 
+    end
+  end
+end
+
 M = {}
-function M.setup()
+function M.setup(cfg)
+  if vim.g.loaded_gruvbox_material == 1 then
+    return
+  end
+  vim.g.loaded_gruvbox_material = 1
+
+  set_default_global_options()
+
   vim.cmd('highlight clear')
-  if vim.fn.exists('syntax_on') then vim.cmd('syntax reset') end
+  if vim.fn.exists('syntax_on') then
+    vim.cmd('syntax reset')
+  end
   vim.o.background = "dark"
   vim.o.termguicolors = true
   vim.g.colors_name = "gruvbox-material"
-  
+
   -- NOTE: I think we can refactor this, but I don't know how
   for group, colors in pairs(highlights.custom()) do
     utils.highlight(group, colors)
@@ -35,3 +65,4 @@ function M.setup()
   end
 end
 return M
+
